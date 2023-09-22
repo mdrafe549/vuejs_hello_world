4a28bf3b61e8de2acc1249f6f17c33699fb0569a
Sayed Rafe <mdrafe549@gmail.com>
2023-09-22 11:21:28 -0700

    first commit

---
+++ src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div id="app">
+    <SampleApp />
+  </div>
+</template>
+
+<script>
+import SampleApp from './components/SampleApp.vue'
+
+export default {
+  name: 'App',
+  components: {
+    SampleApp
+  }
+}
+</script>
+
+<style>
+  @import './assets/styles/global.css';
+</style>
